09f1817b5bd4dbca0b8e51a64541350203ecf83b
fedefeder89 <federico.ruppa@edu.itspiemonte.it>
2023-03-30 11:25:03 +0200

    Struttura base classse Contatti

---
+++ Contatti.py
@@ -0,0 +1,9 @@
+
+class Contatti:
+    def __init__(self):
+        self.__contatti = contatti = [
+        {"nome": "Mario", "cognome": "Rossi", "email": "mario.rossi@example.com", "telefono": "123456789"},
+        {"nome": "Luca", "cognome": "Verdi", "email": "luca.verdi@example.com", "telefono": "987654321"},
+        {"nome": "Simona", "cognome": "Bianchi", "email": "simona.bianchi@example.com", "telefono": "555555555"},
+        {"nome": "Paolo", "cognome": "Neri", "email": "paolo.neri@example.com", "telefono": "333333333"}]
+    
